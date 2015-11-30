00b865ecd5827b47e4ee9b78e65c345cab8300f3
Efrain <jesusayllonroa@gmail.com>
2015-11-30 15:40:03 -0600

    Added controls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,30 +1,5 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//
-//
-// $.getJSON("http://localhost:3000/api/videos")
-//  .done(function(data){
-//  //get the html
-// var htmlTemplate = $(".template").html();
-// // compile into template
-// var compiler = _.template(htmlTemplate);
-// //give template data
-// var htmlified = compiler(data);
-// // add to HTML
-// $("body").append(htmlified);
-// });
