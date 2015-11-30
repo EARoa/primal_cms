b901ff2926a3c39332c94a817e692d1df9b83460
Efrain <jesusayllonroa@gmail.com>
2015-11-29 20:28:44 -0600

    Fixed routes and edited Controller

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,2 +1,22 @@
 class Api::VideosController < ApplicationController
+
+protect_from_forgery with: :reset_session
+
+  def index
+    @videos = Video.all
+    render json: @videos
+  end
+
+  def create
+    @video = Video.new(params.require(:video).permit(:video, :thumbnail, :videourl))
+    @video.save
+    render json: @video
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(params.require(:video).permit(:video, :thumbnail, :videourl))
+    render json: @video
+  end
+
 end
