a15634718b186e15f338b785f42f94c96b3aefaa
Efrain <jesusayllonroa@gmail.com>
2015-11-30 16:24:50 -0600

    Video stops closing modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $('.jsmodal').on('hidden.bs.modal', function () {
+    var $this = $(this).find('video'),
+      tempSrc = $this.attr('src');
+      $this.attr('src', "");
+      $this.attr('src', tempSrc);
+      console.log('Modal hidden, muahaha!');
+    });
+});
