d47aa4f448843eb8a4c1cb7f72b8ff5a4553b13a
Efrain <jesusayllonroa@gmail.com>
2016-01-19 12:32:53 -0600

    Added confirmation to devise users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112190143) do
+ActiveRecord::Schema.define(version: 20160119182805) do
 
   create_table "users", force: :cascade do |t|
     t.boolean  "admin"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20160112190143) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
