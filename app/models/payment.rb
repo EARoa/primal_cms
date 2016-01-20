5777cb4f8dd9f5f058d48e6f654e69a2c76c2488
Efrain <jesusayllonroa@gmail.com>
2016-01-20 12:28:43 -0600

    Subscriptions working

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,11 +12,7 @@ class Payment < ActiveRecord::Base
 
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
-
-    Stripe::Charge.create customer: customer.id,
-                          amount: 2000,
-                          description: 'Premium',
-                          currency: 'usd'
+    customer.subscriptions.create({:plan => "premium"})
   end
 
 end
